57e43e331d27d08c9910a4e38f5ad712f60445d6
Androxus98152 <murzinandrei42@gmail.com>
2023-05-22 21:42:54 +0300

    проверка

--- Bot.py
+++ Bot.py
@@ -3,13 +3,13 @@ import telebot
 from pydub import AudioSegment
 import urllib
 import speech_recognition
-
+import os
 
 # class SpeechRecognition:
 #     pass
 
 
-# Здесь нужно вставить токен, который дал BotFather при регистрации ghj
+# Здесь нужно вставить токен, который дал BotFather при регистрации
 # Пример: token = '2007628239:AAEF4ZVqLiRKG7j49EC4vaRwXjJ6DN6xng8'
 token = '6212964467:AAFDzjpCIWXH-6oII3KKIJRrDMn1E99HylA'
 
@@ -21,22 +21,55 @@ bot = telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def say_hi(message):
     bot.send_message(message.chat.id, 'Рад видеть на моем первом проекте')
-
-# Запускаем бота. Он будет работать до тех пор, пока работает ячейка (крутится значок слева).
-# Остановим ячейку - остановится бот
-bot.polling()
+    # message.chat.first_name("Hello, " + message.chat.first_name)
 
 
 def oga2wav(filename):
     # Переименование формата: 'sample.oga' -> 'sample.wav'
-    new_filename = ...
+    new_filename = filename.replace('.oga', '.wav')
     # Читаем файл с диска с помощью функции AudioSegment.from_file()
-    audio = ...
+    audio = AudioSegment.from_file(filename)
     # Экспортируем файл в новом формате
-    audio.export(..., format=...)
+    audio.export(new_filename, format='wav')
     # Возвратим в качестве результата функции имя нового файла
-    return ...
+    return new_filename
 
-url = "https://drive.google.com/uc?export=view&id=1aBZnHgsjg7XIVlvpYasOOJ8hurp7V6Ww"
-filename = "skillbox_voice_sample.ogg"
-urllib.request.urlretrieve(url, filename)
+def recognize_speech(oga_filename):
+    # Перевод голоса в текст + удаление использованных файлов
+    wav_filename = oga2wav(oga_filename)
+    recognizer = speech_recognition.Recognizer()
+
+    with speech_recognition.WavFile(wav_filename) as source:
+        wav_audio = recognizer.record(source)
+
+    text = recognizer.recognize_google(wav_audio, language='ru')
+
+    if os.path.exists(oga_filename):
+        os.remove(oga_filename)
+
+    if os.path.exists(wav_filename):
+        os.remove(wav_filename)
+
+    return text
+
+
+def download_file(bot, file_id):
+    # Скачивание файла, который прислал пользователь
+    file_info = bot.get_file(file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    filename = file_id + file_info.file_path
+    filename = filename.replace('/', '_')
+    with open(filename, 'wb') as f:
+        f.write(downloaded_file)
+    return filename
+
+
+@bot.message_handler(content_types=['voice'])
+def transcript(message):
+    # Функция, отправляющая текст в ответ на голосовое
+    filename = download_file(bot, message.voice.file_id)
+    text = recognize_speech(filename)
+    bot.send_message(message.chat.id, text)
+
+
+bot.polling()
